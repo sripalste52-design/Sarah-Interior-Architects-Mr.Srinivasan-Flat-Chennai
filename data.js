56a2f8bf7b08a9cd7e1dba9dfb5db70c2089d1a9
sripalste52-design <sripalste.52@gmail.com>
2026-05-10 08:07:27 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,569 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room",
+      "name": "LIVING ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.023561048976453947,
+          "pitch": 0.14907278231460808,
+          "rotation": 0,
+          "target": "1-living-tv-unit"
+        },
+        {
+          "yaw": 0.5036727208483907,
+          "pitch": 0.023216932744798413,
+          "rotation": 1.5707963267948966,
+          "target": "9-son-bed-room-1"
+        },
+        {
+          "yaw": 2.179278912013473,
+          "pitch": 0.09029297467494857,
+          "rotation": 0,
+          "target": "6-kitchen"
+        },
+        {
+          "yaw": 3.12148740978689,
+          "pitch": 0.0225282886062228,
+          "rotation": 0,
+          "target": "4-bolcony"
+        },
+        {
+          "yaw": -1.225566746514124,
+          "pitch": -0.021239685098976224,
+          "rotation": 0,
+          "target": "10-master-bed-room"
+        },
+        {
+          "yaw": 0.3804000866899493,
+          "pitch": -0.1251656679267441,
+          "rotation": 4.71238898038469,
+          "target": "2-living-pooja"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-tv-unit",
+      "name": "LIVING TV UNIT",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7643396141535028,
+          "pitch": 0.030146061218376374,
+          "rotation": 0,
+          "target": "10-master-bed-room"
+        },
+        {
+          "yaw": -2.5599222342109886,
+          "pitch": 0.02450496036044214,
+          "rotation": 1.5707963267948966,
+          "target": "10-master-bed-room"
+        },
+        {
+          "yaw": 2.761174586329833,
+          "pitch": 0.03529845526566611,
+          "rotation": 4.71238898038469,
+          "target": "7-kitchen-crockery"
+        },
+        {
+          "yaw": 3.134334272002725,
+          "pitch": 0.06320977754842616,
+          "rotation": 0,
+          "target": "5-bolcony-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-living-pooja",
+      "name": "LIVING POOJA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0647141451182538,
+          "pitch": 0.026449305856594663,
+          "rotation": 1.5707963267948966,
+          "target": "11-master-bed-room-1"
+        },
+        {
+          "yaw": 0.7747341328934301,
+          "pitch": 0,
+          "rotation": 1.5707963267948966,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": -0.8465774065802485,
+          "pitch": -1.7763568394002505e-15,
+          "rotation": 4.71238898038469,
+          "target": "9-son-bed-room-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-living-",
+      "name": "LIVING ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7018214759594237,
+          "pitch": 0.049448748257235664,
+          "rotation": 1.5707963267948966,
+          "target": "11-master-bed-room-1"
+        },
+        {
+          "yaw": -1.82768062814376,
+          "pitch": 0.020939561771809778,
+          "rotation": 0,
+          "target": "8-son-bed-room"
+        },
+        {
+          "yaw": -0.4913252027794197,
+          "pitch": 0.0026569727819509836,
+          "rotation": 4.71238898038469,
+          "target": "6-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bolcony",
+      "name": "BOLCONY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8600509474931268,
+          "pitch": 0.08274365674888173,
+          "rotation": 1.5707963267948966,
+          "target": "7-kitchen-crockery"
+        },
+        {
+          "yaw": -1.6201664040356043,
+          "pitch": 0.07310050121689571,
+          "rotation": 0,
+          "target": "1-living-tv-unit"
+        },
+        {
+          "yaw": -2.0706210422592477,
+          "pitch": 0.052489642090041144,
+          "rotation": 4.71238898038469,
+          "target": "10-master-bed-room"
+        },
+        {
+          "yaw": 1.4509407241888042,
+          "pitch": -0.1026445643967886,
+          "rotation": 3.141592653589793,
+          "target": "5-bolcony-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bolcony-view",
+      "name": "BOLCONY VIEW",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5762248756848436,
+          "pitch": 0.05308600264496022,
+          "rotation": 4.71238898038469,
+          "target": "10-master-bed-room"
+        },
+        {
+          "yaw": 0.2838247156306668,
+          "pitch": 0.07105342256821778,
+          "rotation": 1.5707963267948966,
+          "target": "8-son-bed-room"
+        },
+        {
+          "yaw": 0.9628273613391336,
+          "pitch": 0.020210705375125926,
+          "rotation": 1.5707963267948966,
+          "target": "7-kitchen-crockery"
+        },
+        {
+          "yaw": 3.1004014679173144,
+          "pitch": -0.21250348049361278,
+          "rotation": 3.141592653589793,
+          "target": "0-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kitchen",
+      "name": "KITCHEN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11693229548533779,
+          "pitch": -0.04385325680314267,
+          "rotation": 3.141592653589793,
+          "target": "7-kitchen-crockery"
+        },
+        {
+          "yaw": 0.7530896726375751,
+          "pitch": -0.03867723514915866,
+          "rotation": 0,
+          "target": "0-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-kitchen-crockery",
+      "name": "KITCHEN CROCKERY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0415188892830205,
+          "pitch": 0.12337835414935938,
+          "rotation": 4.71238898038469,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": 0.015343100490145645,
+          "pitch": 0.012363240045024426,
+          "rotation": 3.141592653589793,
+          "target": "6-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-son-bed-room",
+      "name": "SON BED ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12889080894120042,
+          "pitch": 0,
+          "rotation": 3.141592653589793,
+          "target": "10-master-bed-room"
+        },
+        {
+          "yaw": -3.1318275647942073,
+          "pitch": 0.00007524331592279054,
+          "rotation": 3.141592653589793,
+          "target": "10-master-bed-room"
+        },
+        {
+          "yaw": 2.27214279910706,
+          "pitch": 0.058219573351518505,
+          "rotation": 0,
+          "target": "1-living-tv-unit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-son-bed-room-1",
+      "name": "SON BED ROOM 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.29256084259947635,
+          "pitch": 0.14771246843147878,
+          "rotation": 0,
+          "target": "1-living-tv-unit"
+        },
+        {
+          "yaw": 0.374186362167487,
+          "pitch": 0.13717232909681343,
+          "rotation": 3.141592653589793,
+          "target": "8-son-bed-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-master-bed-room",
+      "name": "MASTER BED ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2171605545844848,
+          "pitch": 0.10069053697722552,
+          "rotation": 1.5707963267948966,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": -0.8708701035399304,
+          "pitch": 0.04460338467308311,
+          "rotation": 3.141592653589793,
+          "target": "11-master-bed-room-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-master-bed-room-1",
+      "name": "MASTER BED ROOM 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.34420257014970446,
+          "pitch": 0.08899372487904955,
+          "rotation": 0,
+          "target": "10-master-bed-room"
+        },
+        {
+          "yaw": -2.1710498134855403,
+          "pitch": 0.17000221979080443,
+          "rotation": 0,
+          "target": "0-living-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "MR.SRINIVASAN",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
